f17939af187036ad8798717bce8c6db89576f8d8
Pawel Klosiewicz <pawelkl@gmail.com>
2012-06-05 15:37:08 +0200

    wxwindows working sample

---
+++ src/main.rb
@@ -0,0 +1,13 @@
+require 'wx'
+require 'rubygems'
+include Wx
+
+class MinimalApp < App
+   def on_init
+    Frame.new(nil, -1, "The Bare Minimum").show()
+   end
+end
+
+MinimalApp.new.main_loop
+
+exit!
